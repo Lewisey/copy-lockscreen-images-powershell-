72dcfecf5eaea9a5f14f468bf10f60ae84c3671a
Lewisey <45331590+Lewisey@users.noreply.github.com>
2019-05-06 23:25:14 +1000

    Update copylockscreenimages.ps1

--- copylockscreenimages.ps1
+++ copylockscreenimages.ps1
@@ -1,7 +1,7 @@
 #Change the username in "$src" to your real one.
 #Change the directory under "$dest" to the one you want to copy to.
 #You may have to create the directory in $dest in order for the images to copy successfully.
-#This script should only copy files with different names to the ones currently populating your chosen $dest directory 
+#This script should only copy files with different names to the ones currently populating your chosen $dest directory.
 
 
 $src = 'C:\Users\#YOURUSERNAMEHERE#\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets'
